dd27c9946babc7178cad1efae5fb1719e202054a
MichaelFred6 <mifred6@gmail.com>
2025-05-26 23:27:00 +0200

    Backend Project

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 // File: models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -9,5 +10,12 @@ const UserSchema = new mongoose.Schema({
   gender: { type: String },
   password: { type: String, required: true }
 }, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 module.exports = mongoose.model('User', UserSchema);
